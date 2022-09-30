0a1e95ee3396483c95f96f691a18911a71789b8e
swjw14 <swjw13@gmail.com>
2022-10-01 01:37:07 +0900

    라인 코딩테스트 3번 문제

---
+++ src/main/kotlin/_라인_3_토템/line_2.kt
@@ -0,0 +1,79 @@
+package _라인_3_토템
+
+import java.lang.Integer.max
+import java.lang.Integer.min
+import kotlin.math.abs
+
+class Solution {
+    fun solution(n: Int, m: Int, fires: List<List<Int>>, ices: List<List<Int>>): List<List<Int>> {
+        val res = MutableList(n) { MutableList(n) { 0 } }
+
+        for (point in fires) {
+            val (row, col) = point
+            val curBoard = MutableList(n) { MutableList(n) { 0 } }
+
+            var minRow = row - 1
+            var maxRow = row - 1
+            var minCol = col - 1
+            var maxCol = col - 1
+
+            // 새로운 point 가 1이 되는 횟수
+            val cnt = maxOf(row - 1, n - row, col - 1, n - col)
+
+            if (cnt >= m) {
+                for (i in 0 until m) {
+                    minRow = max(minRow - 1, 0)
+                    maxRow = min(maxRow + 1, n - 1)
+                    minCol = max(minCol - 1, 0)
+                    maxCol = min(maxCol + 1, n - 1)
+
+                    for (r in minRow..maxRow) {
+                        for (c in minCol..maxCol) {
+                            curBoard[r][c] += 1
+                        }
+                    }
+                }
+                for (r in 0 until n) {
+                    for (c in 0 until n) {
+                        res[r][c] += curBoard[r][c]
+                    }
+                }
+            } else {
+                for (i in 0 until cnt) {
+                    minRow = max(minRow - 1, 0)
+                    maxRow = min(maxRow + 1, n - 1)
+                    minCol = max(minCol - 1, 0)
+                    maxCol = min(maxCol + 1, n - 1)
+
+                    for (r in minRow..maxRow) {
+                        for (c in minCol..maxCol) {
+                            curBoard[r][c] += 1
+                        }
+                    }
+                }
+                for (r in 0 until n) {
+                    for (c in 0 until n) {
+                        res[r][c] += (curBoard[r][c] + (m - cnt))
+                    }
+                }
+            }
+        }
+
+        for (point in ices) {
+            val (row, col) = point
+
+            for (r in 0 until n) {
+                for (c in 0 until n) {
+                    if (r == row - 1 && c == col - 1) {
+                        res[r][c] -= m
+                    } else {
+                        val dist = abs(r - row + 1) + abs(c - col + 1) - 1
+                        res[r][c] -= max(0, m - dist)
+                    }
+                }
+            }
+        }
+
+        return res
+    }
+}
---
+++ src/test/kotlin/_라인_3_토템/SolutionTest.kt
@@ -0,0 +1,47 @@
+package _라인_3_토템
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class SolutionTest {
+    private lateinit var solution: Solution
+
+    @Before
+    fun init() {
+        solution = Solution()
+    }
+
+    @Test
+    fun solution1() {
+        val n = 3
+        val m = 2
+        val fires = listOf(listOf(1, 1))
+        val ices = listOf(listOf(3, 3))
+
+        val result = listOf(
+            listOf(2, 2, 0),
+            listOf(2, 1, -1),
+            listOf(0, -1, -1)
+        )
+        assertEquals(result, solution.solution(n, m, fires, ices))
+    }
+
+    @Test
+    fun solution2() {
+        val n = 5
+        val m = 3
+        val fires = listOf(listOf(5, 5), listOf(1, 3), listOf(5, 2))
+        val ices = listOf(listOf(1, 5), listOf(3, 2))
+
+        val result = listOf(
+            listOf(1, 0, 0, 0, -1),
+            listOf(1, 2, 2, 2, 1),
+            listOf(1, 2, 3, 3, 2),
+            listOf(2, 2, 4, 5, 4),
+            listOf(2, 2, 4, 5, 4)
+        )
+        assertEquals(result, solution.solution(n, m, fires, ices))
+    }
+}
