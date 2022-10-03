469be4cc83c6804f89146a5ab2200c45fff80c78
swjw14 <swjw13@gmail.com>
2022-10-04 01:32:58 +0900

    acm-2467 [용액] 문제 해결

--- src/main/kotlin/_2467_용액/2467.kt
+++ src/main/kotlin/_2467_용액/2467.kt
@@ -1,4 +1,41 @@
 package _2467_용액
 
-class `2467` {
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.abs
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    var minTotalValue = Int.MAX_VALUE
+    var minValue = -1
+    var maxValue = -1
+
+    val n = reader.readLine().toInt()
+    val numberList = reader.readLine().split(" ").map { it.toInt() }.sorted()
+
+    var startIdx = 0
+    var endIdx = n - 1
+
+    while (startIdx < endIdx) {
+        val curTotal = numberList[startIdx] + numberList[endIdx]
+
+        if (abs(curTotal) <= minTotalValue) {
+            minTotalValue = abs(curTotal)
+            minValue = numberList[startIdx]
+            maxValue = numberList[endIdx]
+        }
+
+        if (curTotal < 0) startIdx += 1
+        else endIdx -= 1
+    }
+
+    writer.apply {
+        write("$minValue $maxValue\n")
+        flush()
+        close()
+    }
 }
