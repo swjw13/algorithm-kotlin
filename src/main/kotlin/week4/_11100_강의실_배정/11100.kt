394066677ac4c040660b14bc3ad84e7f10cba9e9
swjw13 <swjw13@gmail.com>
2022-10-20 11:51:13 +0900

    백준 11100 문제 해결

---
+++ src/main/kotlin/week4/_11100_강의실_배정/11100.kt
@@ -0,0 +1,29 @@
+package week4._11100_강의실_배정
+
+import java.util.PriorityQueue
+
+fun main() {
+    val heap: PriorityQueue<List<Int>> = PriorityQueue(java.util.Comparator { o1, o2 ->
+        if ((o1[0] < o2[0]) or (o1[0] == o2[0] && o1[1] < o2[1])) return@Comparator -1
+        else return@Comparator 1
+    })
+    val numberHeap: PriorityQueue<Int> = PriorityQueue()
+
+    with(System.`in`.bufferedReader()) {
+        val n = readLine().toInt()
+        for (i in 0 until n){
+            heap.add(readLine().split(" ").map { it.toInt() })
+        }
+
+        val first = heap.poll()
+        numberHeap.add(first[1])
+        while(heap.size != 0){
+            val curLst = heap.poll()
+            if (curLst[0] >= numberHeap.peek()){
+                numberHeap.poll()
+            }
+            numberHeap.add(curLst[1])
+        }
+        println(numberHeap.size)
+    }
+}
