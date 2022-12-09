dd20f92d2c623c729195c1ea49b79e4d2286650f
swjw13 <swjw13@gmail.com>
2022-12-10 01:22:43 +0900

    백준 1038 해결

---
+++ src/main/kotlin/week11/_1038_감소하는수/1038.kt
@@ -0,0 +1,21 @@
+package week11._1038_감소하는수
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val decreaseNumbers = mutableListOf<Long>()
+
+    fun backTracking(number: String) {
+        decreaseNumbers.add(number.toLong())
+
+        for (i in 0 until number.last().toString().toInt()) {
+            backTracking("$number$i")
+        }
+    }
+
+    for (i in 0 .. 9){
+        backTracking(i.toString())
+    }
+
+    decreaseNumbers.sort()
+    val n = readln().toInt()
+    if (n >= decreaseNumbers.size) println(-1) else println(decreaseNumbers[n])
+}
