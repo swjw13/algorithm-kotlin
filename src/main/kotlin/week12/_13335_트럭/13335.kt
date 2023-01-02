f91be27d77b21796ecbd41123a8a867c08a73ed0
swjw13 <swjw13@gmail.com>
2023-01-02 15:48:06 +0900

    스터디 반영

--- src/main/kotlin/week12/_13335_트럭/13335.kt
+++ src/main/kotlin/week12/_13335_트럭/13335.kt
@@ -7,27 +7,27 @@ class Solution {
         var answer = 0
 
         val bridge: Queue<Int> = LinkedList()
-        for(i in 1..bridge_length) bridge.add(0)
-        var total_weight = 0
+        for (i in 1..bridge_length) bridge.add(0)
+        var totalWeight = 0
 
-        val remain_trucks = truck_weights.toMutableList()
+        val remainTrucks = truck_weights.toMutableList()
 
         while (true) {
-            if (bridge.sum() == 0 && remain_trucks.isEmpty()) break
+            if (bridge.sum() == 0 && remainTrucks.isEmpty()) break
 
             // 큐의 맨 앞 값을 리턴
             if (bridge.element() == 0) {
-                if(remain_trucks.isEmpty()){
+                if (remainTrucks.isEmpty()) {
                     val tmp = bridge.remove()
-                    total_weight -= tmp
+                    totalWeight -= tmp
                     bridge.add(0)
-                } else{
-                    if (total_weight + remain_trucks[0] <= weight) {
+                } else {
+                    if (totalWeight + remainTrucks[0] <= weight) {
                         bridge.remove()
-                        bridge.add(remain_trucks[0])
+                        bridge.add(remainTrucks[0])
 
-                        total_weight += remain_trucks[0]
-                        remain_trucks.removeAt(0)
+                        totalWeight += remainTrucks[0]
+                        remainTrucks.removeAt(0)
 
                     } else {
                         bridge.remove()
@@ -36,21 +36,21 @@ class Solution {
                 }
                 answer += 1
             } else {
-                if(remain_trucks.isEmpty()){
+                if (remainTrucks.isEmpty()) {
                     val tmp = bridge.remove()
-                    total_weight -= tmp
+                    totalWeight -= tmp
                     bridge.add(0)
-                } else{
-                    if(total_weight - bridge.element() + remain_trucks[0] <= weight){
+                } else {
+                    if (totalWeight - bridge.element() + remainTrucks[0] <= weight) {
                         val tmp = bridge.remove()
-                        total_weight -= tmp
+                        totalWeight -= tmp
 
-                        bridge.add(remain_trucks[0])
-                        total_weight += remain_trucks[0]
-                        remain_trucks.removeAt(0)
-                    } else{
+                        bridge.add(remainTrucks[0])
+                        totalWeight += remainTrucks[0]
+                        remainTrucks.removeAt(0)
+                    } else {
                         val tmp = bridge.remove()
-                        total_weight -= tmp
+                        totalWeight -= tmp
                         bridge.add(0)
                     }
                 }
@@ -64,7 +64,7 @@ class Solution {
     }
 }
 
-fun main() = with(System.`in`.bufferedReader()){
+fun main() = with(System.`in`.bufferedReader()) {
     val (_, w, l) = readln().split(" ").map { it.toInt() }
     val lst = readln().split(" ").map { it.toInt() }
 
